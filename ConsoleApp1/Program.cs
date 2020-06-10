806a0f939e35e000099b2c81a0020ffb19c016cb
Bernard828 <bernard.lackey1@gmail.com>
2020-06-10 13:45:36 -0400

    started new line

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             Console.WriteLine("YO!!  123");
             Console.WriteLine("Try #2");
+            Console.WriteLine("Branch 4");
         }
     }
 }
